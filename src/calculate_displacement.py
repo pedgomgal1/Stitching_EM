b7ff20bf319da4a8b3ad5be637b1d4ae5065686c
pedgomgal1 <pgomez-ibis@us.es>
2023-09-28 21:34:28 +0100

    indentaxion fixing

--- src/calculate_displacement.py
+++ src/calculate_displacement.py
@@ -5,22 +5,24 @@ Created on Wed Sep 27 12:44:31 2023
 @author: Pedro Gomez Galvez
 """
 
+
+
 def find_largest_displacement(originalTilesTxtPath, stitchedTilesTxtPath):
     def read_coordinates_from_file(file_path):
         coordinates = {}
         try:
             with open(file_path, "r") as file:
-                for line in file:
-                    parts = [part.strip() for part in line.split(';')]
-                    if len(parts) == 3 and parts[2].startswith('(') and parts[2].endswith(')'):
-                        tile_name = parts[0]
-                        coordinates_str = parts[2][1:-1]  # Remove parentheses
-                        x, y = map(float, coordinates_str.split(','))
-                        coordinates[tile_name] = (x, y)
-        except FileNotFoundError:
-            print("File not found: "+file_path)
-        return coordinates
-
+	       for line in file:
+		    parts = [part.strip() for part in line.split(';')]
+		    if len(parts) == 3 and parts[2].startswith('(') and parts[2].endswith(')'):
+		        tile_name = parts[0]
+		        coordinates_str = parts[2][1:-1]  # Remove parentheses
+		        x, y = map(float, coordinates_str.split(','))
+		        coordinates[tile_name] = (x, y)
+	except FileNotFoundError:
+		print("File not found: "+file_path)
+	return coordinates
+    
     originalTiles_coordinates = read_coordinates_from_file(originalTilesTxtPath)
     stitchedTiles_coordinates = read_coordinates_from_file(stitchedTilesTxtPath)
 
